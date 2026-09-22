6090abaef222dfd747ab080b026d87aee31622e4
Samuel Peixoto <samuelpeixoto2311@gmail.com>
2026-09-22 15:34:39 -0400

    Update main.py
    Adicionando método fazer_aniversario

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ Class Pessoa:
     self.ldade = idade
   def apresentar(self):
     print(f"Olá, meu nome é {self.nome} e {self.idade) anos.")
+  def fazer_aniversario(self):
+    self.idade += 1
 pessoa = Pessoa("Ana", 17)
 pessoa.apresentar()
