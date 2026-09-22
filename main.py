94d36f3cd1d024ef9fdb12964836db7ee78c3579
Samuel Peixoto <samuelpeixoto2311@gmail.com>
2026-09-22 15:44:31 -0400

    Update main.py
    Adicionando método exibir_dados

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ Class Pessoa:
     print(f"Olá, meu nome é {self.nome} e {self.idade) anos.")
   def fazer_aniversario(self):
     self.idade += 1
+  def exibir_dados(self):
+    print(f"Nome: {self.nome}")
+    print(f"Idade: {self.idade}")
+    
 pessoa = Pessoa("Ana", 17)
 pessoa.apresentar()
